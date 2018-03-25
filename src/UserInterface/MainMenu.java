f907741daec4000ee7beac0616b85b13e57c57fd
Serdar Atalay <fserdaratalay@gmail.com>
2018-03-25 23:59:37 +0300

    Updated files

---
+++ src/UserInterface/MainMenu.java
@@ -0,0 +1,22 @@
+package UserInterface;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import javax.swing.plaf.basic.BasicSliderUI;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class MainMenu  extends JPanel{
+
+    private MainMenuPanel mainPanel;
+
+    public MainMenu(){} //Contructor
+}
---
+++ src/UserInterface/MainMenuPanel.java
@@ -0,0 +1,29 @@
+package UserInterface;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import javax.swing.plaf.basic.BasicSliderUI;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javafx.scene.paint.Color;
+
+public class MainMenuPanel extends JPanel implements ActionListener {
+
+    private JButton howToPlayButton;
+    private JButton creditsButton;
+    private JButton optionsButton;
+    private JButton exitButton;
+    private JButton playButton;
+
+    public MainMenuPanel (MouseListener listener)(){}
+
+}
